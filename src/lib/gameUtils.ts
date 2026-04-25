37158eb6d139cb8e9f488d35e26ca57815222f81
Ibukunoluwa Afe <afeibukun@gmail.com>
2026-04-25 16:48:55 +0100

    update word for the day logic

--- src/lib/gameUtils.ts
+++ src/lib/gameUtils.ts
@@ -9,7 +9,15 @@ export const getRandomSolution = (lang: GameLanguage): string => {
 
 export const getDailySolution = (lang: GameLanguage): string => {
     const words = dictionary[lang];
-    const today = new Date().setHours(0, 0, 0, 0); // Normalized timestamp
-    const index = today % words.length;
+    const activeDays = getDaysSinceStart(new Date("2026-04-20"))
+    const index = activeDays % words.length;
     return words[index].toUpperCase();
+};
+
+const getDaysSinceStart = (startDate: Date) => {
+    const today = new Date();
+    // 1. Get difference in milliseconds
+    const diffInMs = today.getTime() - startDate.getTime();
+    // 2. Convert to days and round down
+    return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 };
